fa65c1adb3a3f89dd42c6b856b5e34e15956b654
Scott G. Daniel <scottdaniel25@gmail.com>
2018-03-23 11:39:37 -0700

    start

---
+++ scripts/config.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+#Configuration file of commonly used directories / variables
+
+#this directory
+CWD=$(pwd)
+export SCRIPT_DIR="$CWD"
+export WORKER_DIR="$SCRIPT_DIR/workers"
+
+#project directory, also where .git resides
+export PRJ_DIR="/rsgrps/bhurwitz/scottdaniel/mash-on-patric"
+
+#where singularity images are
+export SING_IMG="/rsgrps/bhurwitz/hurwitzlab/bin"
+
+#where patric genomes are
+export PATRIC_GENOMES="/rsgrps/bhurwitz/hurwitzlab/data/reference/patric_genomes"
+
+#
+#
+# --------------------------------------------------
+function init_dir {
+    for dir in $*; do
+        if [ -d "$dir" ]; then
+            rm -rf $dir/*
+        else
+            mkdir -p "$dir"
+        fi
+    done
+}
+
+# --------------------------------------------------
+function lc() {
+    wc -l $1 | cut -d ' ' -f 1
+}
+
+# --------------------------------------------------
+function get_lines() {
+  FILE=$1
+  OUT_FILE=$2
+  START=${3:-1}
+  STEP=${4:-1}
+
+  if [ -z $FILE ]; then
+    echo No input file
+    exit 1
+  fi
+
+  if [ -z $OUT_FILE ]; then
+    echo No output file
+    exit 1
+  fi
+
+  if [[ ! -e $FILE ]]; then
+    echo Bad file \"$FILE\"
+    exit 1
+  fi
+
+  awk "NR==$START,NR==$(($START + $STEP - 1))" $FILE > $OUT_FILE
+}
+#
+# --------------------------------------------------
+function init_dir {
+    for dir in $*; do
+        if [ -d "$dir" ]; then
+            rm -rf $dir/*
+        else
+            mkdir -p "$dir"
+        fi
+    done
+}
+
+# --------------------------------------------------
+function lc() {
+    wc -l $1 | cut -d ' ' -f 1
+}
+
+# --------------------------------------------------
+function get_lines() {
+  FILE=$1
+  OUT_FILE=$2
+  START=${3:-1}
+  STEP=${4:-1}
+
+  if [ -z $FILE ]; then
+    echo No input file
+    exit 1
+  fi
+
+  if [ -z $OUT_FILE ]; then
+    echo No output file
+    exit 1
+  fi
+
+  if [[ ! -e $FILE ]]; then
+    echo Bad file \"$FILE\"
+    exit 1
+  fi
+
+  awk "NR==$START,NR==$(($START + $STEP - 1))" $FILE > $OUT_FILE
+}
